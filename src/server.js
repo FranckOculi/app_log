b8398a4077f3c0f48b1f2dc233ce9e4f651ebd1c
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-08-16 18:31:49 +0200

    server Update : add env variables

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import App from './app.js'
 
 // Run the server!
-const start = async () => {
+const start = () => {
+	const { SERVER_HOST, SERVER_PORT } = process.env
+
 	try {
-		await App.listen({ port: 4001 })
+		App.listen({ host: SERVER_HOST, port: SERVER_PORT })
 	} catch (err) {
 		App.log.error(err)
 		process.exit(1)
