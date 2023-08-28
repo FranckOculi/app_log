4c0e4199394821d1bb591217676100126c79ea7a
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-08-28 08:28:51 +0200

    server Update : move declaration outside start function

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
-import App from './app.js'
+import App from "./app.js";
+
+const { SERVER_HOST, SERVER_PORT } = process.env;
 
 // Run the server!
-const start = () => {
-	const { SERVER_HOST, SERVER_PORT } = process.env
-
-	try {
-		App.listen({ host: SERVER_HOST, port: SERVER_PORT })
-	} catch (err) {
-		App.log.error(err)
-		process.exit(1)
-	}
-}
-start()
+const start = async () => {
+  try {
+    App.listen({ host: SERVER_HOST, port: SERVER_PORT });
+  } catch (err) {
+    App.log.error(err);
+    process.exit(1);
+  }
+};
+start();
