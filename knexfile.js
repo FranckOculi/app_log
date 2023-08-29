64e18c4979da797baa617e1cff56d6a6471862f0
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-08-29 08:18:10 +0200

    knexfile Update : add dotenv

--- knexfile.js
+++ knexfile.js
@@ -1,31 +1,37 @@
-const config = {
-	development: {
-		client: process.env.DB_CLIENT_DEV,
-		connection: {
-			filename: process.env.DB_PATH_DEV,
-		},
-		useNullAsDefault: true,
-		migrations: {
-			directory: 'database/migrations',
-		},
-		seeds: {
-			directory: 'database/seeds',
-		},
-	},
-	production: {
-		client: process.env.DB_CLIENT,
-		connection: {
-			host: process.env.DB_HOST,
-			port: process.env.DB_PORT,
-			user: process.env.DB_USER,
-			password: process.env.DB_PASSWORD,
-			database: process.env.DB_NAME,
-		},
-		useNullAsDefault: true,
-		migrations: {
-			directory: 'database/migrations',
-		},
-	},
-}
+import dotenv from "dotenv";
+dotenv.config();
 
-export default config
+const config = {
+  development: {
+    client: process.env.DB_CLIENT,
+    connection: {
+      host: process.env.DB_HOST_DEV,
+      user: process.env.DB_USER_DEV,
+      password: process.env.DB_PASSWORD_DEV,
+      database: process.env.DB_NAME_DEV,
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
+  },
+  production: {
+    client: process.env.DB_CLIENT,
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./database/migrations",
+    },
+  },
+};
+
+export default config;
