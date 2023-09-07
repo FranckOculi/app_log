481873d9a44006924b34650fd1c251561dc12cbb
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-09-07 19:36:25 +0200

    app Update : delete js template

--- src/app.js
+++ src/app.js
@@ -1,45 +1,26 @@
-import fastify from 'fastify'
-import cors from '@fastify/cors'
-import fastifyView from '@fastify/view'
-import fastifyStatic from '@fastify/static'
-import ejs from 'ejs'
-import logRoutes from './routes/logRoutes.js'
-import { dirname, join } from 'node:path'
-import { fileURLToPath } from 'node:url'
+import fastify from "fastify";
+import cors from "@fastify/cors";
+import logRoutes from "./routes/logRoutes.js";
 
 const app = fastify({
-	logger: true,
-})
-const rootDir = dirname(dirname(fileURLToPath(import.meta.url)))
+  logger: true,
+});
 
 // cors
 app.register(cors, {
-	origin: true,
-	// origin: configuration.app_url,
-	// allowedHeaders: [
-	//   'Origin',
-	//   'X-Requested-With',
-	//   'Accept',
-	//   'Content-Type',
-	//   'Authorization',
-	// ],
-	// methods: ['GET', 'PUT', 'POST', 'DELETE'],
-})
-
-// templating
-app.register(fastifyView, {
-	engine: {
-		ejs,
-	},
-})
-
-// public
-app.register(fastifyStatic, {
-	root: join(rootDir, 'public'),
-})
+  origin: true,
+  // origin: configuration.app_url,
+  // allowedHeaders: [
+  //   'Origin',
+  //   'X-Requested-With',
+  //   'Accept',
+  //   'Content-Type',
+  //   'Authorization',
+  // ],
+  // methods: ['GET', 'PUT', 'POST', 'DELETE'],
+});
 
 // Routes managers
-app.register(logRoutes, { prefix: '/log' })
-app.setNotFoundHandler((_, res) => res.view('src/views/notFound.ejs'))
+app.register(logRoutes, { prefix: "/log" });
 
-export default app
+export default app;
