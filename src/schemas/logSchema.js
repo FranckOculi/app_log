24a095a1a07ea46f6836c2116569b97a530cf041
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-09-09 14:51:42 +0200

    logSchema Feat

---
+++ src/schemas/logSchema.js
@@ -0,0 +1,30 @@
+import fastJson from "fast-json-stringify";
+
+const bodyJsonSchema = {
+  type: "object",
+  required: ["type", "message", "stack", "email"],
+  properties: {
+    type: { type: "string" },
+    message: { type: "string" },
+    stack: { type: "string" },
+    email: { type: "string" },
+  },
+};
+
+const paramsJsonSchema = {
+  type: "object",
+  properties: {
+    email: { type: "string" },
+    domain: { type: "string" },
+    per_page: { type: "string" },
+    current_page: { type: "string" },
+  },
+};
+
+export const postLogSchema = fastJson({
+  body: bodyJsonSchema,
+});
+
+export const getLogSchema = {
+  query: paramsJsonSchema,
+};
